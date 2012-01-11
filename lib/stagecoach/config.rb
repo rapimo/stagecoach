45a25a77933a10f42926443d5c8f44673a37c4d7
Oli Barnett <o.barnett@digitaleseiten.de>
2012-01-11 18:58:16 +0100

    checks that config info is correct on submission

--- lib/stagecoach/config.rb
+++ lib/stagecoach/config.rb
@@ -2,9 +2,9 @@ require 'yaml'
 
 module Stagecoach
   class Config
-    
+
     @config_file_location = (File.dirname(__FILE__) + '/config.yaml')
-     
+
     def self.open
       File.open(@config_file_location, 'r+')
     end
@@ -18,14 +18,26 @@ module Stagecoach
     end
 
     def self.setup
-      puts "Stagecoach Setup \nEnter your redmine/planio repository, eg. http://digitaleseiten.plan.io:"
-      redmine_repo = STDIN.gets.chomp
-      puts "Enter your API key for that repo:"
-      redmine_api_key = STDIN.gets.chomp
+      CommandLine.line_break
+      puts "Stagecoach Initial Setup"
+      CommandLine.line_break
+      loop do 
+        puts "Enter your redmine/planio repository, eg. http://digitaleseiten.plan.io:"
+        redmine_repo = STDIN.gets.chomp
+        puts "Enter your API key for that repo:"
+        redmine_api_key = STDIN.gets.chomp
 
-      config_hash = {"redmine_site" => redmine_repo, "redmine_api_key"  => redmine_api_key}
+        config_hash = {"redmine_site" => redmine_repo, "redmine_api_key"  => redmine_api_key}
 
-      self.save(config_hash)
+        self.save(config_hash)
+
+        puts "Settings saved OK:"
+        puts "Repository:" + redmine_repo
+        puts "API Key:   " + redmine_api_key
+        puts "Continue? [Y]es or [N]o"
+        break if STDIN.gets.chomp == 'Y'
+        redo if STDIN.gets.chomp == 'N'
+      end 
     end
   end
 end
