a45094f6ea1993f6bc13c3e816735349a5a25eab
Oli Barnett <o.barnett@digitaleseiten.de>
2012-01-11 18:20:42 +0100

    adds -setup flag

--- lib/stagecoach/config.rb
+++ lib/stagecoach/config.rb
@@ -14,6 +14,14 @@ module Stagecoach
       config_file.pos = 0
       config_file.write(hash.to_yaml)
     end
+
+    def self.setup
+      puts "Stagecoach Setup \nEnter your redmine/planio repository, eg. http://digitaleseiten.plan.io:"
+      redmine_repo = STDIN.gets.chomp
+      puts "Enter your API key for that repo:"
+      redmine_api_key = STDIN.gets.chomp
+      self.init(redmine_repo, redmine_api_key)
+    end
   end
 end
 
@@ -22,5 +30,3 @@ class String
   def green; colorize(self, "\e[32m"); end
   def colorize(text, color_code)  "#{color_code}#{text}\e[0m" end
 end
-
-
