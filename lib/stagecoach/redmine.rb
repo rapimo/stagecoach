2db442c337e97c1845dd1a021abdf9fefe815446
Oli Barnett <o.barnett@digitaleseiten.de>
2012-01-22 15:57:06 +0100

    removes pointless test

--- lib/stagecoach/redmine.rb
+++ lib/stagecoach/redmine.rb
@@ -19,17 +19,9 @@ module Stagecoach
     # API testing - can be removed once the redmine API supports changing of
     # issue status, currently this is broken.
     # More information at http://www.redmine.org/boards/2/topics/25920
-    def self.test_issue(issue)
+    def self.view_issue(issue)
       issue_url = Redmine.issue_url(issue)
-      issue_hash = issue.status.attributes
-      if issue_hash['name'] == 'Feedback'
-        puts 'Issue status changed successfully!'
-        puts "View issue: #{issue_url}"
-      else
-        puts "Sorry, couldn't change the issue status for some reason.  Please change it manually:"
-        puts issue_url
-      end
-        print "Open in browser? [Y]es or anything else to exit:  "
+        print "Open planio issue in browser? [Y]es or anything else to exit:  "
         `open #{issue_url.to_s}` if gets.chomp == "Y"
         puts "Staging completed!  Exiting..."
     end
