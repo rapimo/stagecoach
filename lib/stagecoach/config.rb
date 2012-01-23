2604850eb44c2f5fc6ae3e3d31901177486d2e3b
Oli Barnett <o.barnett@digitaleseiten.de>
2012-01-23 19:16:48 +0100

    adds githook installation to setup

--- lib/stagecoach/config.rb
+++ lib/stagecoach/config.rb
@@ -5,7 +5,7 @@ module Stagecoach
     class << self
       def new
         print <<WARNING
-You are running stagecoach from #{`pwd`.chomp}. Is this correct? 
+You are running stagecoach from #{FileUtils.pwd}. Is this correct? 
 #{"Note:".red} stagecoach branch information will be saved here (and .gitignored) [C]ontinue or [Q]uit:
 WARNING
         case STDIN.gets.chomp
@@ -41,8 +41,27 @@ WARNING
         # Create a config file if necessary
         Config.new
 
+        # Install the commit-msg githook if we want to
+        puts "Would you like to install the stagecoach git-hook to automatically reference github issues from each commit?"
+        puts "Note that this will not affect branches not created in stagecoach.  For more information run  stagecoach -h"
+        puts "[I]nstall or [S]kip this step"
+        case STDIN.gets.chomp
+
+        when 'I'
+          source_file = (File.dirname(__FILE__) + '/../githooks/commit-msg')
+          install_dir = FileUtils.pwd + '/.git/hooks'
+          
+          p 'Installing...'
+          FileUtils.cp(source_file, install_dir)
+          p 'Making githook executable (with chmod):'
+          FileUtils.chmod "u+x", ( install_dir + '/commit-msg' )
+
+        when 'S'
+        end
+
         # TODO Some verification of the input at this stage, for example test the
         # connection and have the user re-enter the details if necessary 
+        # http://api.rubyonrails.org/classes/ActiveResource/Connection.html#method-i-head
         loop do 
           print "Enter your redmine/planio repository, eg. https://digitaleseiten.plan.io:  "
           redmine_repo = STDIN.gets.chomp
