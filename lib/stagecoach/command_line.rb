e4d0a8475718926bb893398d3bb0272290eaf092
Oli Barnett <o.barnett@digitaleseiten.de>
2012-01-24 11:39:38 +0100

    githook installation fixes

--- lib/stagecoach/command_line.rb
+++ lib/stagecoach/command_line.rb
@@ -1,7 +1,7 @@
 module Stagecoach
   class CommandLine
     def self.line_break
-      puts  ("-" * 50)
+      puts  "\n"
     end
 
     def self.trollop
@@ -9,20 +9,51 @@ module Stagecoach
       # Command line options using Trollop.
       Trollop::options do
         banner <<-EOS
-Stagecoach works in two stages.  When supplied with an issue number (redmine/planio or github) and a new branch name it essentially runs:
+stagecoach works in two stages, init and deploy. The init stage creates a branch based on a redmine/planio issue, and the deploy stage pushes, merges and (yes!) deploys it. 
 
-  checkout master
-  git pull
-  git checkout -b <new_branch_name>
+#{"You should always run stagecoach from the root directory of your repo".red}
+Otherwise it may (will) break.
+
+The first time you run stagecoach it will ask you for some information, namely your redmine/planio repo URL and your API key for this repo.
+It will also install a custom commit-msg git hook (if you ask it to) - for more information, see below.  
+
+All stagecoach config is saved in /path/to/your/repo/.stagecoach which is created at initial setup and added to your global .gitignore.  This is a yaml file with fairly obvious syntax
+so if you need to remove a branch or edit the issue number that a branch points to, it is possible (although not necessarily recommended) to edit it.
+
+Init Stage
+----------
+stagecoach -p[lanio] 4115 (OR) -g[ithub] 525 -b[ranch] my_new_branch
+
+You can also just run stagecoach without any flags and it will allow you to enter this stuff manually.
+
+To get started, all stagecoach needs from you is the issue number you are working on (redmine/planio or github) and a new branch name.  You /can/ use an existing branch if it 
+is up to date with your master branch. If it is not, stagecoach will squawk and die and you will have to bring the branch up to date, or use a new one.
   
-If you are working from a redmine/planio issue, it assigns that issue to you and sets it to 'In Progress'.
-It also sets up a git issue for you to reference in your commits.
+If you are working from a redmine/planio issue, stagecoach sets the issue to 'In Progress'.  Currently it does not assign the issue to you, but you have the option to view the issue
+in your browser and do this manually.  It also sets up a git issue for you to reference in your commits (see commit-msg githook).
 
-If you are working from a github issue, it assigns that issue to you.
+If you are working from a github issue, we can all get on with our lives.
 
-Then you can code and commit, code and commit until your feature or fix is ready.
+Coding Stage
+------------
+Future versions of stagecoach may do the coding for you, but at the moment you have to do this part manually.
+Just code and commit, code and commit until your feature or fix is ready.
 
-Once this is done you can run stagecoach -d[eploy]
+#{"commit-msg githook".green}
+If you opt to install the commit-msg githook during initial setup (stagecoach -s) then your commit messages will be automatically referenced to the github issue of the branch you are in 
+(this only applies to branches created or registered in Stagecoach).
+
+  - to reference a different issue from a commit, simply refer to it as normal with #xxx in the commit message. The git-hook will leave your message alone.
+  - to make no reference at all, you can use the #noref tag in the commit message. The git-hook
+  - to close an issue with a commit, use the #closes tag.
+
+For more information, see 
+  - the githook itself at /path/to/your/repo/.git/hooks/commit-msg
+  - http://book.git-scm.com/5_git_hooks.html
+
+Deploy Stage
+------------
+stagecoach -d[eploy]
 
 This automates the entire deploy workflow for you as follows:
 
@@ -30,8 +61,9 @@ This automates the entire deploy workflow for you as follows:
   git checkout staging
   git pull
   git merge task_name
-  set redmine/planio ticket to 'feedback' status if applicable
-  submits a pull request on Github
+  git push origin staging
+  cap staging deploy
+  set redmine/planio ticket to 'feedback' status (if applicable)
  
 #{"Sample usage:".green}
   stagecoach -p 4115 -b new_branch_name
--- lib/stagecoach/config.rb
+++ lib/stagecoach/config.rb
@@ -1,13 +1,10 @@
 require 'yaml'
+require 'FileUtils'
 
 module Stagecoach
   class Config
     class << self
       def new
-        print <<WARNING
-You are running stagecoach from #{FileUtils.pwd}. Is this correct? 
-#{"Note:".red} stagecoach branch information will be saved here (and .gitignored) [C]ontinue or [Q]uit:
-WARNING
         case STDIN.gets.chomp
         when 'C'
           File.open(CONFIG_FILE, 'w') { |f| f.write("---\nredmine_site: none\nredmine_api_key: none")}
@@ -18,7 +15,7 @@ WARNING
       end
 
       def open
-        File.open(CONFIG_FILE, 'a+')
+        File.open(CONFIG_FILE, 'r+')
       end
 
       def yaml_to_hash 
@@ -29,40 +26,78 @@ WARNING
         config_file.write(hash.to_yaml)
       end
 
+      def githook_install(source_dir, install_dir, file)
+        FileUtils.cp(source_dir + file, install_dir + file)
+        puts 'OK!'
+        puts 'Making githook executable (may require admin password)'
+        FileUtils.chmod(0711, ( install_dir + file ))
+        puts 'OK!'
+      end
+
       def setup
         # Say hello
         CommandLine.line_break
         puts "Stagecoach Initial Setup"
         CommandLine.line_break
 
-        # Ignore the stagecoach config file
-        Git.global_ignore('.stagecoach')
+        # Now scare everybody away again
+        puts "You are running stagecoach from #{FileUtils.pwd.green}. Is this the root directory of your repository?" 
+        puts "Stagecoach may not work properly anywhere else! So proceed with caution"
+        CommandLine.line_break
+        print "[C]ontinue or [Q]uit:"
 
-        # Create a config file if necessary
+        # Create a config file if necessary (Config.new deals with the C or Q input for now although it is ugly)
         Config.new
 
-        # Install the commit-msg githook if we want to
-        puts "Would you like to install the stagecoach git-hook to automatically reference github issues from each commit?"
-        puts "Note that this will not affect branches not created in stagecoach.  For more information run  stagecoach -h"
+        # Tell git to ignore the stagecoach config file
+        Git.global_ignore('.stagecoach')
+
+        # Install the commit-msg githook if it is not already there:
+        source_dir = (File.dirname(__FILE__) + '/../githooks/')
+        install_dir = FileUtils.pwd + '/.git/hooks/'
+        git_hook = 'commit-msg'  
+
+        CommandLine.line_break
+        puts "Would you like to install the stagecoach #{"commit-msg githook".green}?"
+        puts "This automatically references stagecoach-created github issues from each commit you make"
+        puts "Note that this will only affect branches created in stagecoach.  For more information run stagecoach -h"
+        CommandLine.line_break
         puts "[I]nstall or [S]kip this step"
-        case STDIN.gets.chomp
-
-        when 'I'
-          source_file = (File.dirname(__FILE__) + '/../githooks/commit-msg')
-          install_dir = FileUtils.pwd + '/.git/hooks'
-          
-          p 'Installing...'
-          FileUtils.cp(source_file, install_dir)
-          p 'Making githook executable (with chmod):'
-          FileUtils.chmod "u+x", ( install_dir + '/commit-msg' )
-
-        when 'S'
+        loop do
+          case STDIN.gets.chomp
+          when 'I'
+            if File.exist?(install_dir + git_hook)
+              case FileUtils.compare_file(source_dir + git_hook, install_dir + git_hook) 
+              when true
+                puts 'The stagecoach githook is already installed in this repo. Skipping this step...'
+                break
+              when false
+                puts "You have a commit-msg githook already.  Are you sure you want to install?  This will #{'overwrite'.red} your current commit-msg githook."
+                print "Type [overwrite] to continue or anything else to skip installation:"
+                case STDIN.gets.chomp
+                when 'overwrite'
+                  Config.githook_install(source_dir, install_dir, git_hook)
+                  break
+                else
+                  break
+                end
+              end
+            else
+              puts "Installing..."
+              Config.githook_install(source_dir, install_dir, git_hook)
+              break
+            end
+          when 'S'
+            puts 'Skipping Installation.'
+            break
+          end
         end
 
         # TODO Some verification of the input at this stage, for example test the
         # connection and have the user re-enter the details if necessary 
         # http://api.rubyonrails.org/classes/ActiveResource/Connection.html#method-i-head
         loop do 
+          CommandLine.line_break
           print "Enter your redmine/planio repository, eg. https://digitaleseiten.plan.io:  "
           redmine_repo = STDIN.gets.chomp
           print "Enter your API key for that repo:  "
